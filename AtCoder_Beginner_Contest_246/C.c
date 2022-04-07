3fe7e58b47b3cd077adbc562e32017bb01039b53
yuki_shimoda <example@example.com>
2022-04-07 18:05:27 +0900

    C.c

--- AtCoder_Beginner_Contest_246/C.c
+++ AtCoder_Beginner_Contest_246/C.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(void)
 {
-	scanf()
+	int n, k, x;
+	int *a;
+	int i, j;
+
+	i = 0;
+	j = 0;
+	scanf("%d %d %d", n, k, x);
+	a = (int *)malloc(sizeof(int) * n);
+	while (i < n)
+	{
+		scanf("%d",a[i]);
+		i++;
+	}
+	while (k > 0)
+	{
+		while (a[j] >= x && k > 0)
+		{
+			a[j] -= x;
+			k--;
+		}
+		j++;
+	}
+	
+	
+
+	
 }
