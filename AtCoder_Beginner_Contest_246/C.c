af1fefb65abc55ac741cade72a02b4f3b37b6398
yuki_shimoda <example@example.com>
2022-04-09 17:57:07 +0900

    C.c

--- AtCoder_Beginner_Contest_246/C.c
+++ AtCoder_Beginner_Contest_246/C.c
@@ -4,29 +4,41 @@
 int	main(void)
 {
 	int n, k, x;
-	int *a;
-	int i, j;
+	int i, j, sum;
 
 	i = 0;
 	j = 0;
-	scanf("%d %d %d", n, k, x);
-	a = (int *)malloc(sizeof(int) * n);
+	sum = 0;
+	scanf("%d %d %d", &n, &k, &x);
+	int a[n];
+	// a = (int *)malloc(sizeof(int) * n);
+	//store numbers
 	while (i < n)
 	{
 		scanf("%d",a[i]);
+		// a++;
 		i++;
 	}
-	while (k > 0)
+
+	while (k > 0 && j < n)
 	{
-		while (a[j] >= x && k > 0)
+		while (a[j] >= x && k > 0 && j < n)
 		{
 			a[j] -= x;
 			k--;
 		}
 		j++;
 	}
-	
-	
 
-	
+	if (k > n)
+		return 0;
+
+	while (j > 0)
+	{
+		sum += a[j];
+	}
+	printf("%ld", sizeof(*a)/sizeof(a[0]));
+	printf("%d", sum);
+
+	free(a);
 }
