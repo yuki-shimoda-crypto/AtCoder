4f464a427308add7e2927bcb2a0dc42eb37419c0
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-04-19 18:57:53 +0900

    2_05

--- APG4B/2_05.c
+++ APG4B/2_05.c
@@ -64,43 +64,58 @@
 // 	return (0);	
 // }
 
+// #include <stdio.h>
+
+// int is_prime_from_two(int n, int i, int *flag)
+// {
+// 	if (n / i < i)
+// 		return 0;
+// 	if (n % i == 0)
+// 	{
+// 		*flag = 0;
+// 		return 0;
+// 	}
+// 	return is_prime_from_two(n, i + 1, flag);
+// }
+
+// int is_prime(int n)
+// {
+// 	int flag;
+
+// 	flag = 1;
+// 	if (n < 2)
+// 		return 0;
+// 	else if (n == 2)
+// 		return 1;
+// 	is_prime_from_two(n, 2, &flag);
+// 	if (flag == 1)
+// 		return 1;
+// 	return 0;
+// }
+
+// int main(void)
+// {
+// 	int n;
+
+// 	scanf("%d", &n);
+// 	if (is_prime(n))
+// 		printf("%d is prime\n", n);
+// 	else
+// 		printf("%d is not prime\n", n);
+// 	return (0);
+// }
+
 #include <stdio.h>
 
-int is_prime_from_two(int n, int i, int *flag)
+char *reverse_array(char *str)
 {
-	if (n / i <= i)
-		return 0;
-	if (n % i == 0)
-	{
-		*flag = 0;
-		return 0;
-	}
-	return is_prime_from_two(n, i + 1, flag);
-}
+	int i;
+	i = 0;
 
-int is_prime(int n)
-{
-	int *flag;
-
-	if (n < 2)
-		return 0;
-	else if (n == 2)
-		return 1;
-	*flag = 1;
-	is_prime_from_two(n, 2, flag);
-	if (*flag == 1)
-		return 1;
-	return 0;
+	
 }
 
 int main(void)
 {
-	int n;
 
-	scanf("%d", &n);
-	if (is_prime(n))
-		printf("%d is prime\n", n);
-	else
-		printf("%d is not prime\n", n);
-	return (0);
 }
