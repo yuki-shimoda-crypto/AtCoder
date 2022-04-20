fcaccdc4f12c60f7f2bdca393feebb8675aa3a1b
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-04-20 21:47:36 +0900

    2_05

--- APG4B/2_05.c
+++ APG4B/2_05.c
@@ -105,17 +105,81 @@
 // 	return (0);
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-char *reverse_array(char *str)
-{
-	int i;
-	i = 0;
+// char *reverse_array(char *str)
+// {
+// 	int i;
+// 	i = 0;
 
 	
-}
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	int n, *p;
+	int i;
+
+	scanf("%d", &n);
+	p = (int *)malloc(sizeof(int) * n);
+	i =  0;
+	while (i < n - 1)
+	{
+		scanf("%d", p[i]);
+		i++;
+	}
+	
 
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+    // x番の組織が親組織に提出する枚数を返す
+    // childrenは組織の関係を表す2次元配列(参照渡し)
+    int count_report_num(vector<vector<int>> &children, int x) {
+      // ベースケース
+      if (children.at(x).size() == 0) {
+        // 子組織から受け取ることは無いので1枚であることが確定している
+        return 1;
+      }
+     
+      // 再帰ステップ
+      int sum = 0;
+      for (int c : children.at(x)) {
+        sum += count_report_num(children, c);
+      }
+      sum += 1;  // x番の組織の報告書の枚数(1枚)を足す
+      return sum;
+    }
+     
+    // これ以降の行は変更しなくてよい
+     
+    int main(void) {
+      int N;
+      scanf("%d", &N);
+     
+      p[n];  // 各組織の親組織を示す配列
+      p[0] = -1;  // 0番組織の親組織は存在しないので-1を入れておく
+      for (int i = 1; i < N; i++) {
+        cin >> p.at(i);
+		scanf("%d", &p);
+      }
+     
+      // 組織の関係から2次元配列を作る
+      vector<vector<int>> children(N);  // ある組織の子組織の番号一覧
+      for (int i = 1; i < N; i++) {
+        int parent = p.at(i);  // i番の親組織の番号
+        children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
+      }
+     
+      // 各組織について、答えを出力
+      for (int i = 0; i < N; i++) {
+        cout << count_report_num(children, i) << endl;
+      }
+    }
