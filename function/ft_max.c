85142cdaeb71b9f3bf361737da147e8b7fa76259
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-04-25 16:41:11 +0900

    function

--- function/ft_max.c
+++ function/ft_max.c
@@ -1,8 +1,14 @@
 // Return the greater of a and b
 int ft_max(int a, int b)
 {
-	if (a > b)
-		return a;
-	else
-		return b;
+	return (a > b) ? a : b;
 }
+
+// int ft_max(int a, int b)
+// {
+// 	if (a > b)
+// 		return a;
+// 	else
+// 		return b;
+// }
+
--- function/ft_min.c
+++ function/ft_min.c
@@ -1,8 +1,13 @@
 // Return the smaller of a and b
 int ft_min(int a, int b)
 {
-	if (a < b)
-		return a;
-	else
-		return b;
+	return (a < b) ? a : b;
 }
+
+// int ft_min(int a, int b)
+// {
+// 	if (a < b)
+// 		return a;
+// 	else
+// 		return b;
+// }
