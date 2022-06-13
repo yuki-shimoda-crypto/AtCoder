3806f6aecfc1a2fd4ee76c6d51b6fba52d168f2a
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-06-13 23:55:24 +0900

    ABC_251

---
+++ ABC_251/a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+int main(void)
+{
+	char str[4];
+	int len = 0;
+	int num = 0;
+	int i = 0;
+
+	scanf("%s", str);
+	for (int i = 0; str[i] != '\0'; i++)
+		len++;
+	while (num < 6)
+	{
+		printf("%s", str);
+		num += len;  
+	}
+		printf("\n");
+		
+    return (0);
+}
