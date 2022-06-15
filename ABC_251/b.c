15e313e31ae40a6d68e29313583d9d75904f7e9c
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-06-15 18:43:25 +0900

    ABC_251

---
+++ ABC_251/b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+int main(void)
+{
+	long n, w;
+	scanf("%ld%ld", &n, &w);
+	long a[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("ld", &a[i]);
+	}
+	
+    return (0);
+}
