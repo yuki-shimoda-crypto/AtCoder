48122b38704eaccf18e4e872e15e9db513726e40
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-06-16 17:12:02 +0900

    ABC_251

--- ABC_251/b.c
+++ ABC_251/b.c
@@ -6,14 +6,56 @@
 
 int main(void)
 {
-	long n, w;
-	scanf("%ld%ld", &n, &w);
-	long a[n];
+	int n, w;
+	scanf("%d%d", &n, &w);
+	int a[n];
+	int flag[w + 1];
+	int num;
+	int ans;
+
+	for (int i = 0; i < w + 1; i++)
+		flag[i] = 0;
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	
+	for (int i = 0; i < n; i++)
+	{
+		num = a[i];
+		if (num <= w)
+			flag[num] = 1;
+	}
 
 	for (int i = 0; i < n; i++)
 	{
-		scanf("ld", &a[i]);
+		for (int j = i + 1; j < n; j++)
+		{
+			num = a[i] + a[j];
+			if (num <= w)
+				flag[num] = 1;
+		}
 	}
-	
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			for (int k = j + 1; k < n; k++)
+			{
+				num = a[i] + a[j] + a[k];
+				if (num <= w)
+					flag[num] = 1;
+			}
+		}
+	}
+
+	ans = 0;
+	for (int i = 1; i < w + 1; i++)
+	{
+		if (flag[i] == 1)
+			ans++;
+	}
+	printf("%d\n", ans);
+
     return (0);
 }
