928e63dac63b47c3b2f7f030634e42da38ed19bf
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-06-19 15:30:43 +0900

    rename

---
+++ abc250/A.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+int main(void)
+{
+	int h, w, r, c;
+	
+    return (0);
+}
--- abc256/c.c
+++ abc256/c.c
@@ -196,51 +196,88 @@
 //     return (0);
 // }
 
+// int main(void)
+// {
+// 	int flag_h1 = 0, flag_h2 = 0, flag_h3 = 0;
+// 	int flag_w1 = 0, flag_w2 = 0, flag_w3 = 0;
+// 	int count = 0;
+
+// 	int h1, h2, h3, w1, w2, w3;
+// 	scanf("%d%d%d%d%d%d", &h1, &h2, &h3, &w1, &w2, &w3);
+
+// 	for (int i = 1; i <= 28; i++)//1
+// 	{
+// 		for (int j = 1; j <= 28; j++)//2
+// 		{
+// 			for (int k = 1; k <= 28; k++)//3
+// 			{
+// 				if (i + j + k != h1)
+// 					continue;
+// 				for (int l = 1; l <= 28; l++)//4
+// 				{
+// 					for (int m = 1; m <= 28; m++)//5
+// 					{
+// 						for (int n = 1; n <= 28; n++)//6
+// 						{
+// 							if (l + m + n != h2)
+// 								continue;
+// 							for (int o = 1; o <= 28; o++)//7
+// 							{
+// 								if (i + l + o != w1)
+// 									continue;
+// 								for (int p = 1; p <= 28; p++)//8
+// 								{
+// 									if (j + m + p != w2)
+// 										continue;
+// 									for (int q = 1; q <= 28; q++)//9
+// 									{
+// 										if (o + p + q != h3)
+// 											continue;
+// 										if (k + n + q != w3)
+// 											continue;
+// 										count++;
+// 									}
+// 								}
+// 							}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	printf("%d\n", count);
+//     return (0);
+// }
+
+#define MIN(a, b) (a - b > 0 ? b: a)
+
 int main(void)
 {
-	int flag_h1 = 0, flag_h2 = 0, flag_h3 = 0;
-	int flag_w1 = 0, flag_w2 = 0, flag_w3 = 0;
+	int h1, h2, h3, w1, w2, w3;
+	int a, b, c;
+	int d, e, f;
+	int g, h, i;
 	int count = 0;
 
-	int h1, h2, h3, w1, w2, w3;
 	scanf("%d%d%d%d%d%d", &h1, &h2, &h3, &w1, &w2, &w3);
 
-	for (int i = 1; i <= 28; i++)//1
+	for (a = 1; a <= 28; a++)//1
 	{
-		for (int j = 1; j <= 28; j++)//2
+		for (b = 1; b <= 28; b++)//2
 		{
-			for (int k = 1; k <= 28; k++)//3
+			c = h1 - a - b;
+			if (c < 1)
+				continue;
+			for (d = 1; d <= 28; d++)//4
 			{
-				if (i + j + k != h1)
-					continue;
-				for (int l = 1; l <= 28; l++)//4
+				for (e = 1; e <= 28; e++)//5
 				{
-					for (int m = 1; m <= 28; m++)//5
-					{
-						for (int n = 1; n <= 28; n++)//6
-						{
-							if (l + m + n != h2)
-								continue;
-							for (int o = 1; o <= 28; o++)//7
-							{
-								if (i + l + o != w1)
-									continue;
-								for (int p = 1; p <= 28; p++)//8
-								{
-									if (j + m + p != w2)
-										continue;
-									for (int q = 1; q <= 28; q++)//9
-									{
-										if (o + p + q != h3)
-											continue;
-										if (k + n + q != w3)
-											continue;
-										count++;
-									}
-								}
-							}
-						}
-					}
+					f = h2 - d - e;
+					g = w1 - a - d;
+					h = w2 - b - e;
+					i = w3 - c - f;
+					if(MIN(f, MIN(g, (MIN(h, i)))) > 0 && g + h + i == h3)
+						count++;
 				}
 			}
 		}
