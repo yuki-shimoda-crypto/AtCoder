8c1d5d1872349102afb642a22abe8a1498890ded
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-06-25 14:54:37 +0900

    abc250

--- abc250/B.c
+++ abc250/B.c
@@ -1,3 +1,4 @@
+// 解答見た
 #include <limits.h>
 #include <math.h>
 #include <stdbool.h>
@@ -5,46 +6,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+void ft_tiles(int n, char **tiles)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (j % 2 == 0)
+					tiles[i][j] = '.';
+				else
+					tiles[i][j] = '#';
+			}
+			
+			
+		}
+		else
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (j % 2 == 0)
+					tiles[i][j] = '#';
+				else
+					tiles[i][j] = '.';
+			}
+			
+		}
+	}
+	return ;
+}
+
 int main(void)
 {
 	int n, a, b;
+	char **tiles;
 	char **table;
-	bool flag = false;
 
 	scanf("%d%d%d", &n, &a, &b);
+
+	tiles = (char **)malloc(sizeof(char *) * n);
+	for (int i = 0; i < n; i++)
+		tiles[i] = (char *)malloc(sizeof(char) * (n + 1));
+	
 	table = (char **)malloc(sizeof(char *) * a * n);
 	for (int i = 0; i < a * n; i++)
+		table[i] = (char *)malloc(sizeof(char) * (b * n + 1));
+
+	ft_tiles(n, tiles);
+
+	for (int i = 0; i < a * n; i++)
 	{
-		*table = (char *)malloc(sizeof(char) * (b * n + 1));
+		for (int j = 0; j < b * n; j++)
+			table[i][j] = tiles[i / a][j / b];
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < a * n; i++)
 	{
-		for (int j = 0; j < a; j++)
-		{
-			for (int k = 0; k < n; k++)
-			{
-				for (int l = 0; l < b; l++)
-				{
-					if (flag)
-					{
-						printf("#");
-					}
-					else
-					{
-						printf(".");
-					}
-				}
-				flag = flag ? false : true;
-			}
-			printf("\n");		
-		}
-		if ((n % 2 == 0))
-		{
-			flag = flag ? false : true;
-		}
-		
+		printf("%s\n", table[i]);
 	}
-	
 	return (0);
 }
