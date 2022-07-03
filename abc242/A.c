27c178333d7749afa40da9f493548841179ebd41
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-07-03 23:53:42 +0900

    abx242

---
+++ abc242/A.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	double a, b, c, x;
+
+	scanf("%lf%lf%lf%lf", &a, &b, &c, &x);
+
+	if (/* condition */)
+	{
+		/* code */
+	}
+	
+	printf("%lf", (a + c)/ )
+	return (0);
+}
