adde96592598fa753372ecfa0bde4677ccdb8243
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-07-18 15:30:54 +0900

    function

---
+++ function/ft_stack.c
@@ -0,0 +1,25 @@
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct s_stack
+{
+	int max;
+	int ptr;
+	int *stk;
+}		t_stack;
+
+int ft_stack_alloc(s_stack *stk, int max)
+{
+	
+}
+
+
+int main(void)
+{
+
+	return (0);
+}
