3a23c4e40d28737a75a9b59fd62bbad2706fee71
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-08-23 13:15:03 +0900

    commit

---
+++ abc241/B.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	long long	n, m;
+	scanf("%lld%lld", &n, &m);
+
+	long long	a[n], b[m];
+	for (int i = 0; i < n; i++)
+		scanf("%lld", &a[i]);
+	for (int i = 0; i < n; i++)
+	
+	return (0);
+}
