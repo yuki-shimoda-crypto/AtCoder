8f4f393cbef2182b482a91e8d5f1168fa346dd2c
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-10-22 15:39:16 +0900

    add: rank A: abc226

---
+++ abc226/abc226_a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int	main(void) {
+	int	n, m;
+	int a, b, c, d, e, f;
+	int	ans = 0;
+	string	s, t;
+	double	x;
+
+	cin >> x;
+	cout << (int)round(x) << endl;
+	return (0);
+}
