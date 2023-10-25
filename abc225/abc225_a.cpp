fa78bb2694b274f09be504781325b16fb98c4f76
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-10-25 15:11:36 +0900

    add: rank A: abc224

--- abc225/abc225_a.cpp
+++ abc225/abc225_a.cpp
@@ -1,19 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// 
+// int	main(void) {
+// 	int	n, m;
+// 	int a, b, c, d, e, f;
+// 	int	ans = 0;
+// 	string	s, t;
+// 
+// 	cin >> s;
+// 	if (s[0] != s[1] && s[1] != s[2] && s[0] != s[2]) {
+// 		cout << 6 << endl;
+// 	} else if ((s[0] == s[1] && s[1] != s[2]) || (s[0] != s[1] && s[1] == s[2]) || (s[0] != s[1] && s[0] == s[2])) {
+// 		cout << 3 << endl;
+// 	} else {
+// 		cout << 1 << endl;
+// 	}
+// 	return (0);
+// }
+
 #include <iostream>
+#include <set>
+#include <algorithm>
 using namespace std;
 
 int	main(void) {
 	int	n, m;
 	int a, b, c, d, e, f;
 	int	ans = 0;
-	string	s, t;
+	string	S;
+	set<string> s;
 
-	cin >> s;
-	if (s[0] != s[1] && s[1] != s[2] && s[0] != s[2]) {
-		cout << 6 << endl;
-	} else if ((s[0] == s[1] && s[1] != s[2]) || (s[0] != s[1] && s[1] == s[2]) || (s[0] != s[1] && s[0] == s[2])) {
-		cout << 3 << endl;
-	} else {
-		cout << 1 << endl;
+	cin >> S;
+	sort(S.begin(), S.end());
+	while (1) {
+		s.insert(S);
+		if (!next_permutation(S.begin(), S.end())) {
+			break ;
+		}
 	}
+	cout << s.size() << endl;
 	return (0);
 }
