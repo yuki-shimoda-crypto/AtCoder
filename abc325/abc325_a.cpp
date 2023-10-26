f762fa1c28e3992daf9454918740efa6ede918c0
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-10-26 14:47:01 +0900

    add: rank A: abc325

---
+++ abc325/abc325_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int	main(void) {
+	int	n, m;
+	int a, b, c, d, e, f;
+	int	ans = 0;
+	string	s, t;
+
+	cin >> s >> t;
+	cout << s << " san" << endl;
+	return (0);
+}
